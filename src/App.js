81125601b11d5376c268533b9b22972928a87aae
feden2906 <feden2906@gmail.com>
2021-07-07 16:21:36 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Button from "./components/button/Button";
-
+import Users from "./components/users/Users";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-      <Button />
+      <>
+        <Button />
+        <Route path={'/users'} component={Users} />
+      </>
   );
 }
 
--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,7 +1,7 @@
 export default function Post({singlePost}){
     return(
         <div>
-            <h3>Post:</h3>
+            <h3>Post: {singlePost.id}</h3>
             <p><strong>Title: </strong>{singlePost.title}</p>
             <p><strong>Body: </strong>{singlePost.body}</p>
         </div>
