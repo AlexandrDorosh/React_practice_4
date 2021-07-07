f17f6a6b3952d442b200f1818b650dce618fa89c
AlexandrDorosh <dovgaloleksandr388@gmail.com>
2021-07-07 15:43:24 +0300

    fixed but have some problems

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,10 +1,22 @@
 import Post from "../post/Post";
+import {useEffect, useState} from "react";
+import {getPost} from "../../services/Api";
+import {useParams} from 'react-router-dom';
 
-export default function Posts({postsList}){
+export default function Posts(){
+    const {id} = useParams();
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        getPost(id).then(value => {
+            setPosts([...value.data])
+        })
+    }, [id])
+
     return(
         <div>
             {
-                postsList.map(value => <Post key={value.id} singlePost={value} />)
+                posts.map(value => <Post key={value.id} singlePost={value} />)
             }
         </div>
     );
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,27 +1,17 @@
 import './User.css';
 import {Link, Route} from "react-router-dom";
 import Posts from "../posts/Posts";
-import {useState} from "react";
-import {getPost} from "../../services/Api";
 
 export default function User({item, userId}){
-    const [posts, setPosts] = useState([])
-
-    const getPosts = () => {
-        getPost(item.id).then(value => {
-            setPosts([...value.data])
-        })
-    }
 
     return(
         <div className={'user'}>
             {item.name} - <Link to={'/users/' + item.id} onClick={() => {
                 userId(item.id)
-                getPosts()
             }
         } >show posts</Link>
 
-            <Route path={'/users/' + item.id} render={() => <Posts item={item} postsList={posts} />}/>
+            <Route path={'/users/' + item.id} render={() => <Posts />}/>
         </div>
     )
 }
